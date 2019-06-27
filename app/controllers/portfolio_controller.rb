024e0aee782af9e7c1f0e2792879da3528164875
koo-ke <hope14kou@hotmail.co.jp>
2019-06-27 16:49:50 +0900

    投稿のBASIC認証を削除/アプリ全体に反映

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -5,7 +5,7 @@ class PortfolioController < ApplicationController
   end
 
   def new
-    basic_auth
+
   end
 
   def edit
@@ -33,12 +33,6 @@ class PortfolioController < ApplicationController
   end
 
   private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
 
   def posts_params
     params.permit(:apptitle, :image, :github, :text)
