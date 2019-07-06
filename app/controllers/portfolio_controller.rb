4974eefe5947152b55e97a5ef534e0e16a922f91
koo-ke <hope14kou@hotmail.co.jp>
2019-07-06 12:00:18 +0900

    投稿機能にベーシック認証

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -5,7 +5,7 @@ class PortfolioController < ApplicationController
   end
 
   def new
-    http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
+    basic_auth
   end
 
   def edit
@@ -36,6 +36,14 @@ class PortfolioController < ApplicationController
 
   def posts_params
     params.permit(:apptitle, :image, :github, :text).merge(user_id: current_user.id)
+  end
+
+  def basic_auth
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      end
+    end
   end
 
 end
