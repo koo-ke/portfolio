1385ba77f94f9c949886506eecd91ae5af967db4
koo-ke <hope14kou@hotmail.co.jp>
2019-07-06 12:05:39 +0900

    環境変数が間違っていた為、修正

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -41,7 +41,7 @@ class PortfolioController < ApplicationController
   def basic_auth
     if Rails.env.production?
       authenticate_or_request_with_http_basic do |username, password|
-        username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+        username == ENV["BASIC_AUTH_USERNAME"] && password == ENV["BASIC_AUTH_PASSWORD"]
       end
     end
   end
